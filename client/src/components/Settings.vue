56f0beff0530a3fc0e8cc2a59973e0d10f615965
Shelly Huang <shellyhuang81@gmail.com>
2019-06-02 18:50:48 -0400

    create settings vuex storage

--- client/src/components/Settings.vue
+++ client/src/components/Settings.vue
@@ -1,29 +1,27 @@
 <template>
-    <div>
-        <input 
-        type='number' 
-        min='1' max='6' 
-        name='num_players'
-        v-model="num_players"/>
+  <div>
+    <input
+    type='number'
+    min='1' max='6'
+    name='num_players'
+    v-model="numPlayers"/>
 
-        <button @click="submit"></button>
-    </div>
+    <button @click="submit">Submit</button>
+  </div>
 </template>
 
 <script>
-// import { settings } from '../store/settings.js'
+import { settings } from '../store/settings.js'
 export default {
-    data() {
-        return{
-            num_players: 2
-        }
-    },
-    methods: {
-        submit(){
-            // console.log('submit form ')
-            // settings.commit('setNumPlayers', num_players)
-        }
+  data () {
+    return {
+      numPlayers: 2
     }
+  },
+  methods: {
+    submit () {
+      settings.commit('setNumPlayers', this.numPlayers)
+    }
+  }
 }
 </script>
-
--- client/src/store/settings.js
+++ client/src/store/settings.js
@@ -1,15 +1,18 @@
 import VueX from 'vuex'
+import Vue from 'vue'
+Vue.use(VueX)
 
 export const settings = new VueX.Store({
   state: {
-    numPlayers: 0
+    numPlayers: 0,
+    numTokens: 0
   },
   mutations: {
-    setNumPlayers (state, num) {
-      state.numPlayers = num
-    }
+    setNumPlayers (state, num) { state.numPlayers = num },
+    setNumTokens (state, num) { state.numTokens = num }
   },
   getters: {
-    numPlayers: state => state.numPlayers
+    numPlayers: state => state.numPlayers,
+    numTokens: state => state.numTokens
   }
 })
