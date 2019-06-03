89e0a653dfcbd3989fa838bddd944f0547650c5c
Shelly Huang <shellyhuang81@gmail.com>
2019-06-03 18:01:36 -0400

    create tokenCostCalculation function to assign cost of pick/ban

--- client/src/components/Token.vue
+++ client/src/components/Token.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <TokensLeft/>
-    <TokenChoices :cost=1 choice='pick'/>
-    <TokenChoices :cost=1 choice='ban'/>
+    <TokenChoices :cost='pickCost' choice='pick'/>
+    <br/>
+    <TokenChoices :cost='banCost' choice='ban'/>
   </div>
 </template>
 
@@ -11,9 +12,22 @@ import TokensLeft from './Token-TokensLeft.vue'
 import TokenChoices from './Token-Choices.vue'
 
 export default {
+  data () {
+    return {
+      pickCost: 1,
+      banCost: 1
+    }
+  },
   components: {
     TokensLeft,
     TokenChoices
+  },
+  computed: {
+    // adjust token costs here
+    tokenCalculation: () => {
+      this.pickCost = 1
+      this.banCost = 1
+    }
   }
 }
 </script>
