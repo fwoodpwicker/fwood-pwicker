a1918db1f8bc1b50387af5f2eed149dccf05d987
Shelly Huang <shellyhuang81@gmail.com>
2019-06-06 23:40:22 -0400

    change to input on edit icon click for player's name

--- client/src/components/Settings-DisplayNames.vue
+++ client/src/components/Settings-DisplayNames.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
+  <div @click='exitEditing()'>
     <span v-for="i in names" v-bind:key="i">
-      <span>{{i}}</span> <img @click='updateName' height=15 width=15 src='../../public/edit-icon.png'>
+      <span v-if='!editing[i]'>{{i}}</span>
+      <input v-else type='text' v-model='editName'>
+      <img @click='updatingName(i)' height=15 width=15 src='../../public/edit-icon.png'>
       <br/>
     </span>
   </div>
@@ -13,18 +15,25 @@ export default {
   data () {
     return {
       names: [],
-      edit: false
+      editing: {}
     }
   },
   methods: {
     updatePlayers () {
       this.names = []
+      this.clearEditing()
+
       for (let i = 1; i <= this.value; i++) {
         this.names.push('Player ' + i)
+        // use $set for reactive change
+        this.$set(this.editing, this.names[i - 1], false)
       }
     },
-    updateName () {
-      console.log('updating')
+    updatingName (index) {
+      this.$set(this.editing, index, true)
+    },
+    clearEditing () {
+      Object.keys(this.editing).forEach(i => delete this.editing[i])
     }
   },
   watch: {
