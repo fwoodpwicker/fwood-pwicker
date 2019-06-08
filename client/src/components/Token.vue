ab128d536f351bd94bb12b7400707e8629d97e5e
Shelly Huang <shellyhuang81@gmail.com>
2019-06-08 02:56:51 -0400

    emit to children current user in store

--- client/src/components/Token.vue
+++ client/src/components/Token.vue
@@ -5,14 +5,15 @@
     <br/>
     <TokenChoices :cost='banCost' choice='ban'/>
 
-    <button @click='singleSubmit'>Next</button>
+    <button v-if='currPlayer' @click='previousPlayer'>Previous</button>
+    <button @click='nextPlayer'>Next</button>
   </div>
 </template>
 
 <script>
 import TokensLeft from './Token-TokensLeft.vue'
 import TokenChoices from './Token-Choices.vue'
-// import { playerChoiceStore } from '../store/player-choices.js'
+import { playerChoiceStore } from '../store/player-choices.js'
 import { EventBus } from '../main.js'
 
 export default {
@@ -21,6 +22,7 @@ export default {
     return {
       pickCost: 1,
       banCost: 1,
+      currPlayer: 0,
       numPicks: 0,
       numBans: 0
     }
@@ -30,46 +32,41 @@ export default {
     TokenChoices
   },
   computed: {
-    // adjust num tokens given to each player
-    totalTokenCalculation: () => {
-
-      // change 10
-      // settings.commit('setTotalTokens', 10)
-    },
-    // adjust pick/ban costs here
-    choiceCostCalculation: () => {
-
+    getCurrUser: function () {
+      return Object.keys(playerChoiceStore.state)[this.currPlayer]
     }
+    // // adjust num tokens given to each player
+    // totalTokenCalculation: function () {
+
+    //   // change 10
+    //   // settings.commit('setTotalTokens', 10)
+    // },
+    // // adjust pick/ban costs here
+    // choiceCostCalculation: function () {
+
+    // }
   },
   methods: {
-    // for single person
-    singleSubmit () {
-      console.log('dec made', this.decisionsMade)
-      // get values form other components x
-
-      // 1. put picks and bans into storage
-      // 2. put choices into temporary variable
-
-      // 1- put each person into separate modules
-      // let playerRegistration = {
-      //   state: {
-      //     name: 'something random',
-
-      //     numPicks: this.numPicks,
-      //     numBans: this.numBans
-      //   }
-      // }
-
-      // playerChoiceStore.registerModule('player'+this.decisionsMade, playerRegistration)
-
-      // if registered everyone's decisions
-      if (this.decisionsMade) this.$router.push({ name: 'pick', params: { decisionsMade: this.decisionsMade - 1 } })
-      else console.log('onto next route')
+    previousPlayer () {
+      this.$set(this.currPlayer, this.currPlayer - 1)
+      this.emitCurrPlayer()
+    },
+    nextPlayer () {
+      if (this.currPlayer !== Object.keys(playerChoiceStore.state).length - 1) {
+        this.$set(this.currPlayer, this.currPlayer + 1)
+        this.emitCurrPlayer()
+      } else {
+        console.log('go to pick page')
+      }
+    },
+    emitCurrPlayer () {
+      console.log(Object.keys(playerChoiceStore.state)[this.currPlayer])
+      EventBus.$emit('get-curr-user', Object.keys(playerChoiceStore.state)[this.currPlayer])
     }
   },
   mounted () {
     // this.totalTokenCalculation()
-
+    this.emitCurrPlayer()
     // get number of picks/bans from components from Token total-tokens component
     EventBus.$on('numPicks', (picks) => { this.numPicks = picks })
     EventBus.$on('numBans', (bans) => { this.numBans = bans })
