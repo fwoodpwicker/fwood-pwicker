ea947aed65e6baf7abe1e568d455532b00694136
Shelly Huang <shellyhuang81@gmail.com>
2019-06-10 01:03:43 -0400

    change current user displayed based on store

--- client/src/components/Token.vue
+++ client/src/components/Token.vue
@@ -22,9 +22,7 @@ export default {
     return {
       pickCost: 1,
       banCost: 1,
-      currPlayer: 0,
-      numPicks: 0,
-      numBans: 0
+      currPlayer: 0
     }
   },
   components: {
@@ -32,15 +30,6 @@ export default {
     TokenChoices
   },
   computed: {
-    getCurrUser: function () {
-      return Object.keys(playerChoiceStore.state)[this.currPlayer]
-    }
-    // // adjust num tokens given to each player
-    // totalTokenCalculation: function () {
-
-    //   // change 10
-    //   // settings.commit('setTotalTokens', 10)
-    // },
     // // adjust pick/ban costs here
     // choiceCostCalculation: function () {
 
@@ -48,28 +37,23 @@ export default {
   },
   methods: {
     previousPlayer () {
-      this.$set(this.currPlayer, this.currPlayer - 1)
+      this.currPlayer--
       this.emitCurrPlayer()
     },
     nextPlayer () {
       if (this.currPlayer !== Object.keys(playerChoiceStore.state).length - 1) {
-        this.$set(this.currPlayer, this.currPlayer + 1)
+        this.currPlayer++
         this.emitCurrPlayer()
       } else {
         console.log('go to pick page')
       }
     },
     emitCurrPlayer () {
-      console.log(Object.keys(playerChoiceStore.state)[this.currPlayer])
       EventBus.$emit('get-curr-user', Object.keys(playerChoiceStore.state)[this.currPlayer])
     }
   },
   mounted () {
-    // this.totalTokenCalculation()
     this.emitCurrPlayer()
-    // get number of picks/bans from components from Token total-tokens component
-    EventBus.$on('numPicks', (picks) => { this.numPicks = picks })
-    EventBus.$on('numBans', (bans) => { this.numBans = bans })
   }
 }
 </script>
