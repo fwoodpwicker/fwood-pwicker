ec4bcfd4d439ebb8f34b2a3ab04b291abff371c4
Shelly Huang <shellyhuang81@gmail.com>
2019-06-11 17:32:47 -0400

    add field to add picks for session

--- client/src/components/Pick.vue
+++ client/src/components/Pick.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    Pick page
+    Input a place/category: <input v-model= 'inputPick' type='text'>
+    <button @click='addPick'>Add</button>
   </div>
 </template>
 <script>
+// import { playerChoiceStore } from '../store/player-choices.js'
+
 export default {
+  data () {
+    return {
+      currPlayer: 0,
+      inputPick: '',
+      picks: []
+    }
+  },
+  methods: {
+    addPick () {
+      this.picks.push(this.inputPick)
+      console.log(this.picks)
+    }
+  }
 }
 </script>
-
