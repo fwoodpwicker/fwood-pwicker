bf5f6fe48393e003cedbdfd6f2a47ab597d5ff38
Shelly Huang <shellyhuang81@gmail.com>
2019-06-12 13:19:21 -0400

    update picks that are added to add to screen

--- client/src/components/Pick.vue
+++ client/src/components/Pick.vue
@@ -2,6 +2,11 @@
   <div>
     Input a place/category: <input v-model= 'inputPick' type='text'>
     <button @click='addPick'>Add</button>
+    <br/>
+    <span v-for="(item, index) in picks" v-bind:key="index">
+      {{ item }} <img @click='removePick(index)' width=15 height=15 src='../assets/exit-icon.jpg'>
+      <br/>
+    </span>
   </div>
 </template>
 <script>
@@ -18,8 +23,17 @@ export default {
   methods: {
     addPick () {
       this.picks.push(this.inputPick)
-      console.log(this.picks)
+      this.updatePicks()
+    },
+    updatePicks () {
+      for (let i = 0; i < this.picks.length; i++) {
+        this.$set(this.picks, i, this.picks[i])
+      }
+    },
+    removePick (index) {
+      // remove element by index
     }
+    
   }
 }
 </script>
